7f9c7fd0305a92117970cc7c1ac0c7d2fdb40546
luis7523 <luismi0101@gmail.com>
2016-04-05 11:41:31 -0500

    cosa esta de Pi

--- Neuron.py
+++ Neuron.py
@@ -5,7 +5,7 @@
 import numpy as np
 from scipy.interpolate import interp1d
 import matplotlib
-import matplotlibe.pyplot as plt
+import matplotlib.pyplot as plt
 
 delta = 0.001
 V = -65
@@ -18,9 +18,46 @@ GNa = 120
 Vk = -77
 h0 = 0.6
 m0 = 0.05 
-phi = 3 ** ((t-6.3)/10)
-Bn = phi * (0.125 * (np.exp(V/20)))
-An = phi * (0.01 * (V + 10)/((np.exp(V+10/10)-1)))
-Am = phi * (0.01 * (V + 25)/((np.exp(V+25/10)-1)))
-n1 = (An * (1-n0) - Bn * n0) * delta + n0
+phi = 3 ** ((T-6.3)/10)
+
+
+def An(v):
+	return phi * ((0.01 * V + 10))/(np.exp (((V+10)/10)- 1))
+def Am(v):
+	return phi * ((0.01 * V + 25))/(np.exp (((V+25)/10)- 1))
+def Ah(v):
+	return phi * (0.07 * np.exp(v/29))
+
+
+def Bn(v):
+	return phi * (0.125 * np.exp(v/80))
+def Bm(v):
+	return phi * (4 * np.exp(v/18))
+def Bh(v):
+	return phi * (1/(np.exp((v+30)/10)+1))
+
+	
+def Fn(n):
+	return An(V0) * (1-n) - Bn(V0)*n
+def Fm(m):
+	return Am(V0) * (1-m) - Bm(V0)*m
+def Fh(h):
+	return Ah(V0) * (1-h) - Bh(V0)*h
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ Pi.py~
@@ -0,0 +1,9 @@
+#Luis Miguel Parra Rueda
+import numpy as np
+L = 3
+R = 1
+N = 
+for i in range (100):
+	x = np.random.random()
+	y = np.random.random()
+
